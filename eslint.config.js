2ad72f3390277140f7148b76aaced5b893f9f94f
Jablo-cmd <jabuloysiba@gmail.com>
2026-09-24 22:37:11 +0200

    chore: add eslint configuration

---
+++ eslint.config.js
@@ -0,0 +1,1 @@
+import js from "@eslint/js"; import globals from "globals"; import tseslint from "typescript-eslint"; export default tseslint.config({ignores:["dist","node_modules"],extends:[js.configs.recommended,...tseslint.configs.recommended],files:["**/*.{ts,tsx}"],languageOptions:{globals:{...globals.browser,...globals.node}},rules:{}});
